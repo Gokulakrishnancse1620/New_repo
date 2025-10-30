493f7bfb95247e05dc689cfb09a7f9ca9527b7ab
Gokulakrishnancse1620 <gokulakrishnancse1620sce@gmail.com>
2025-10-30 20:10:09 +0530

    Add two methods of Array Leaders

--- arrayleaders.java
+++ arrayleaders.java
@@ -3,35 +3,30 @@ public class arrayleaders{
 	public static void main(String[] args){
 		int[] arr={16, 17, 4, 3, 5, 2};
 		int len=arr.length;
-		// int max=-1;
-		// for(int i=len-1;i>=0;i--){
-		// 	max=Math.max(arr[i],arr[i]=max);
-		// }
-		// for(int i:arr)
-		// 	System.out.println(i);
 		ArrayList<Integer> ans=new ArrayList<>();
-		int maxval=-1;
-		//System.out.println(len);
-		for(int i=0;i<len;i++){
+		/*for(int i=0;i<len;i++){//method 1
 			int j;
 			for(j=i+1;j<len;j++){
 				if(arr[i]<arr[j]){
 					break;
 				}
-				//System.out.println(arr[i]);
-				//System.out.println(arr[j]);
-
-				// if(arr[i]<arr[j]){
-				// 	break;
-				// }
-				// if(j==len){
-				// 	ans.add(arr[i]);
-				// }
 			}
 			if(j==len){
 				ans.add(arr[i]);
 			}
+		}*/
+		//method 2
+		int large_value=arr[arr.length-1];
+		ans.add(large_value);
+		for(int i=arr.length-2;i>=0;i--){
+			if(arr[i]>large_value){
+				large_value=arr[i];
+				ans.add(large_value);
+			}
 		}
+		Collections.reverse(ans);
+
 		System.out.println(ans);
+
 	}
 }
